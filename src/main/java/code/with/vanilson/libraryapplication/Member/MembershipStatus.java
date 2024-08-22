936e4e443b0191956defd38e148c251c9ab98c93
vanilson muhongo <vanilsonmuhongo@gmail.com>
2024-08-22 21:00:34 +0100

    Create Entity Class for Member #10
    - Defined the Member entity with fields like id, membershipDate, borrowedBooks, etc.
    
    - Inherit from Person.

---
+++ src/main/java/code/with/vanilson/libraryapplication/Member/MembershipStatus.java
@@ -0,0 +1,19 @@
+package code.with.vanilson.libraryapplication.Member;
+
+/**
+ * MembershipStatus
+ *
+ * @author vamuhong
+ * @version 1.0
+ * @since 2024-08-22
+ */
+public enum MembershipStatus {
+    ACTIVE,
+    INACTIVE,
+    EXPIRED,
+    CANCELLED,
+    PENDING_APPROVAL,
+    PENDING_RENEWAL,
+    PENDING_REDEMPTION,
+    SUSPEND;
+}
