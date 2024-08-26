613344f3c461bc911aec0d5a2bcbaec600a6913e
vanilson muhongo <vanilsonmuhongo@gmail.com>
2024-08-26 12:06:16 +0100

    - Create JPA repositories for All entities to handle database operations.
    - MemberRepository
    - BookRepository
    - AdminRepository
    - LibrarianRepository
    - MemberRepository
    - FineRepository

---
+++ src/main/java/code/with/vanilson/libraryapplication/admin/AdminRepository.java
@@ -0,0 +1,16 @@
+package code.with.vanilson.libraryapplication.admin;
+
+import code.with.vanilson.libraryapplication.book.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * BookRepository
+ *
+ * @author vamuhong
+ * @version 1.0
+ * @since 2024-08-26
+ */
+@Repository
+public interface AdminRepository extends JpaRepository<Admin, Long> {
+}
